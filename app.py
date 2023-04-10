3e28eedf76809741aefc865159b476fccca6c4af
thomas-yanxin <yx20001210@163.com>
2023-04-10 14:09:10 +0800

    init

---
+++ app.py
@@ -0,0 +1,104 @@
+import gradio as gr
+from langchain.chains import ChatVectorDBChain
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain.prompts.chat import (ChatPromptTemplate,
+                                    HumanMessagePromptTemplate,
+                                    SystemMessagePromptTemplate)
+from langchain.prompts.prompt import PromptTemplate
+from langchain.vectorstores import FAISS
+
+from chatglm_llm import ChatGLM
+
+
+def init_knowledge_vector_store(filepath):
+    embeddings = HuggingFaceEmbeddings(
+        model_name="GanymedeNil/text2vec-large-chinese", )
+    loader = UnstructuredFileLoader(filepath, mode="elements")
+    docs = loader.load()
+
+    vector_store = FAISS.from_documents(docs, embeddings)
+    return vector_store
+
+
+def get_knowledge_based_answer(query, vector_store, chat_history=[]):
+    system_template = """基于以下内容，简洁和专业的来回答用户的问题。
+    如果无法从中得到答案，请说 "不知道" 或 "没有足够的相关信息"，不要试图编造答案。答案请使用中文。
+    ----------------
+    {context}
+    ----------------
+    """
+    messages = [
+        SystemMessagePromptTemplate.from_template(system_template),
+        HumanMessagePromptTemplate.from_template("{question}"),
+    ]
+    prompt = ChatPromptTemplate.from_messages(messages)
+
+    condese_propmt_template = """任务: 给一段对话和一个后续问题，将后续问题改写成一个独立的问题。确保问题是完整的，没有模糊的指代。
+    ----------------
+    聊天记录：
+    {chat_history}
+    ----------------
+    后续问题：{question}
+    ----------------
+    改写后的独立、完整的问题："""
+    new_question_prompt = PromptTemplate.from_template(condese_propmt_template)
+    chatglm = ChatGLM()
+    chatglm.history = chat_history
+    knowledge_chain = ChatVectorDBChain.from_llm(
+        llm=chatglm,
+        vectorstore=vector_store,
+        qa_prompt=prompt,
+        condense_question_prompt=new_question_prompt,
+    )
+
+    knowledge_chain.return_source_documents = True
+    knowledge_chain.top_k_docs_for_context = 10
+
+    result = knowledge_chain({"question": query, "chat_history": []})
+    return result
+
+
+def clear_session():
+    return '', '', None
+
+
+def predict(input, file_obj, history=None):
+    if history == None:
+        history = []
+    print(file_obj.name)
+    vector_store = init_knowledge_vector_store(file_obj.name)
+
+    resp = get_knowledge_based_answer(query=input,
+                                               vector_store=vector_store)
+    history.append((input, resp['answer']))
+    return '', history, history
+
+
+
+if __name__ == "__main__":
+    block = gr.Blocks()
+    with block as demo:
+        gr.Markdown("""<h1><center>LangChain-ChatGLM-weibu</center></h1>
+        <center><font size=3><a href='https://modelscope.cn/models/ZhipuAI/ChatGLM-6B/summary' target="_blank">ChatGLM-6B </a>是一个开源的、支持中英双语的对话语言模型，基于 General Language Model (GLM) 架构，具有 62 亿参数。</center></font>
+        """)
+        chatbot = gr.Chatbot(label='ChatGLM-6B')
+        message = gr.Textbox()
+        state = gr.State()
+        file = gr.File()
+        message.submit(predict,
+                       inputs=[message, file, state],
+                       outputs=[message, chatbot, state])
+        with gr.Row():
+            clear_history = gr.Button("🧹 清除历史对话")
+            send = gr.Button("🚀 发送")
+
+            send.click(predict,
+                       inputs=[message, file, state],
+                       outputs=[message, chatbot, state])
+            clear_history.click(fn=clear_session,
+                                inputs=[],
+                                outputs=[chatbot],
+                                queue=False)
+
+    demo.queue().launch(height=800, share=True)
---
+++ chatglm_llm.py
@@ -0,0 +1,60 @@
+from typing import List, Optional
+
+import torch
+from langchain.llms.base import LLM
+from langchain.llms.utils import enforce_stop_tokens
+from transformers import AutoModel, AutoTokenizer
+
+DEVICE = "cuda"
+DEVICE_ID = "0"
+CUDA_DEVICE = f"{DEVICE}:{DEVICE_ID}" if DEVICE_ID else DEVICE
+
+
+def torch_gc():
+    if torch.cuda.is_available():
+        with torch.cuda.device(CUDA_DEVICE):
+            torch.cuda.empty_cache()
+            torch.cuda.ipc_collect()
+
+
+tokenizer = AutoTokenizer.from_pretrained(
+    "THUDM/chatglm-6b",
+    trust_remote_code=True
+)
+model = (
+    AutoModel.from_pretrained(
+        "THUDM/chatglm-6b",
+        trust_remote_code=True)
+    .half()
+    .cuda()
+)
+
+
+class ChatGLM(LLM):
+    max_token: int = 10000
+    temperature: float = 0.1
+    top_p = 0.9
+    history = []
+
+    def __init__(self):
+        super().__init__()
+
+    @property
+    def _llm_type(self) -> str:
+        return "ChatGLM"
+
+    def _call(self,
+              prompt: str,
+              stop: Optional[List[str]] = None) -> str:
+        response, updated_history = model.chat(
+            tokenizer,
+            prompt,
+            history=self.history,
+            max_length=self.max_token,
+            temperature=self.temperature,
+        )
+        torch_gc()
+        if stop is not None:
+            response = enforce_stop_tokens(response, stop)
+        self.history = updated_history
+        return response
