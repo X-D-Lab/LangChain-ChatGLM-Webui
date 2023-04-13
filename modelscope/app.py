980577ab14c248c1e00c7b58cdd7d8e84ce6be57
thomas-yanxin <yx20001210@163.com>
2023-04-13 11:48:37 +0800

    Update app.py

--- modelscope/app.py
+++ modelscope/app.py
@@ -3,16 +3,15 @@ import os
 import nltk
 
 nltk.download('averaged_perceptron_tagger')
+import os
+
 import gradio as gr
 import sentence_transformers
 import torch
-from langchain.chains import ChatVectorDBChain
+from langchain.chains import RetrievalQA
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-from langchain.prompts.chat import (ChatPromptTemplate,
-                                    HumanMessagePromptTemplate,
-                                    SystemMessagePromptTemplate)
-from langchain.prompts.prompt import PromptTemplate
+from langchain.prompts import PromptTemplate
 from langchain.vectorstores import FAISS
 
 from chatglm_llm import ChatGLM
@@ -28,82 +27,81 @@ embedding_model_dict = {
 
 
 def init_knowledge_vector_store(embedding_model, filepath):
+
     embeddings = HuggingFaceEmbeddings(
         model_name=embedding_model_dict[embedding_model], )
+    embeddings.client = sentence_transformers.SentenceTransformer(
+        embeddings.model_name, device=DEVICE)
+
     loader = UnstructuredFileLoader(filepath, mode="elements")
     docs = loader.load()
+
     vector_store = FAISS.from_documents(docs, embeddings)
     return vector_store
 
 
-def get_knowledge_based_answer(query, vector_store, chat_history=[]):
-    print('get_knowledge_based_answer')
-    system_template = """基于以下内容，简洁和专业的来回答用户的问题。
-    如果无法从中得到答案，请说 "不知道" 或 "没有足够的相关信息"，不要试图编造答案。答案请使用中文。
-    ----------------
-    {context}
-    ----------------
-    """
-    messages = [
-        SystemMessagePromptTemplate.from_template(system_template),
-        HumanMessagePromptTemplate.from_template("{question}"),
-    ]
-    prompt = ChatPromptTemplate.from_messages(messages)
+def get_knowledge_based_answer(query,
+                               vector_store,
+                               VECTOR_SEARCH_TOP_K,
+                               chat_history=[]):
+    prompt_template = """基于以下已知信息，简洁和专业的来回答用户的问题。
+        如果无法从中得到答案，请说 "根据已知信息无法回答该问题" 或 "没有提供足够的相关信息"，不允许在答案中添加编造成分，答案请使用中文。
 
-    condese_propmt_template = """任务: 给一段对话和一个后续问题，将后续问题改写成一个独立的问题。确保问题是完整的，没有模糊的指代。
-    ----------------
-    聊天记录：
-    {chat_history}
-    ----------------
-    后续问题：{question}
-    ----------------
-    改写后的独立、完整的问题："""
-    new_question_prompt = PromptTemplate.from_template(condese_propmt_template)
+        已知内容:
+        {context}
+
+        问题:
+        {question}"""
+    prompt = PromptTemplate(template=prompt_template,
+                            input_variables=["context", "question"])
+
     chatLLM = ChatGLM()
-    knowledge_chain = ChatVectorDBChain.from_llm(
+    knowledge_chain = RetrievalQA.from_llm(
         llm=chatLLM,
-        vectorstore=vector_store,
-        qa_prompt=prompt,
-        condense_question_prompt=new_question_prompt,
-    )
+        retriever=vector_store.as_retriever(
+            search_kwargs={"k": VECTOR_SEARCH_TOP_K}),
+        prompt=prompt)
 
     knowledge_chain.return_source_documents = True
-    knowledge_chain.top_k_docs_for_context = 10
-
-    result = knowledge_chain({"question": query, "chat_history": []})
-    print(result)
-    return result
+    result = knowledge_chain({"query": query})
+    print(result['result'])
+    return result['result']
 
 
 def clear_session():
     return '', None
 
 
-def predict(input, embedding_model, file_obj, history=None):
-
+def predict(input,
+            embedding_model,
+            file_obj,
+            VECTOR_SEARCH_TOP_K,
+            history=None):
+    print('predict')
     if history == None:
         history = []
     print(file_obj.name)
     vector_store = init_knowledge_vector_store(embedding_model, file_obj.name)
 
-    resp = get_knowledge_based_answer(query=input, vector_store=vector_store)
-    history.append((input, resp['answer']))
+    resp = get_knowledge_based_answer(query=input,
+                                      vector_store=vector_store,
+                                      VECTOR_SEARCH_TOP_K=VECTOR_SEARCH_TOP_K)
+    history.append((input, resp))
     return '', history, history
 
 
 if __name__ == "__main__":
     block = gr.Blocks()
     with block as demo:
-        gr.Markdown("""<h1><center>LangChain-ChatLLM-Webui</center></h1>
-        <center><font size=3>
-        本项目基于LangChain和大型语言模型系列模型, 提供基于本地知识的自动问答应用. <br>
-        目前项目提供基于<a href='https://github.com/THUDM/ChatGLM-6B' target="_blank">ChatGLM-6B </a>的LLM和包括GanymedeNil/text2vec-large-chinese、nghuyong/ernie-3.0-base-zh、nghuyong/ernie-3.0-nano-zh在内的多个Embedding模型, 支持上传 txt、docx、md 等文本格式文件. <br>
-        后续将提供更加多样化的LLM、Embedding和参数选项供用户尝试, 欢迎关注<a href='https://github.com/thomas-yanxin/LangChain-ChatGLM-Webui' target="_blank">Github地址</a>.
+        gr.Markdown("""<h1><center>LangChain-ChatGLM-Webui</center></h1>
+        <center><font size=3><a href='https://github.com/THUDM/ChatGLM-6B' target="_blank">ChatGLM-6B </a>是一个开源的、支持中英双语的对话语言模型，基于 General Language Model (GLM) 架构，具有 62 亿参数. <br>
+        本项目利用LangChain和ChatGLM-6B系列模型制作Webui, 提供基于本地知识的大模型应用. <br>
+        目前支持上传 txt、docx、md 等文本格式文件.
         </center></font>
         """)
         with gr.Row():
             with gr.Column(scale=4):
-                chatbot = gr.Chatbot(label='ChatLLM').style(height=300)
+                chatbot = gr.Chatbot(label='ChatLLM').style(height=400)
                 message = gr.Textbox(label='请输入问题')
                 state = gr.State()
 
@@ -113,12 +111,22 @@ if __name__ == "__main__":
                     label="Embedding model",
                     value="ernie-tiny")
                 file = gr.File(label='请上传知识库文件',
-                               file_types=['.txt', '.md', '.docx'])
+                               file_types=['.txt', '.md',
+                                           '.docx']).style(height=100)
+                VECTOR_SEARCH_TOP_K = gr.Slider(1,
+                                                20,
+                                                value=6,
+                                                step=1,
+                                                label="vector search top k",
+                                                interactive=True)
                 clear_history = gr.Button("🧹 清除历史对话")
                 send = gr.Button("🚀 发送")
 
                 send.click(predict,
-                           inputs=[message, embedding_model, file, state],
+                           inputs=[
+                               message, embedding_model, file,
+                               VECTOR_SEARCH_TOP_K, state
+                           ],
                            outputs=[message, chatbot, state])
                 clear_history.click(fn=clear_session,
                                     inputs=[],
@@ -126,6 +134,14 @@ if __name__ == "__main__":
                                     queue=False)
 
         message.submit(predict,
-                       inputs=[message, embedding_model, file, state],
+                       inputs=[
+                           message, embedding_model, file, VECTOR_SEARCH_TOP_K,
+                           state
+                       ],
                        outputs=[message, chatbot, state])
+        gr.Markdown("""<font size=3>**提醒**：<br>
+        1. 请勿上传或输入敏感内容，否则输出内容将被平台拦截返回error. <br>
+        2. 使用时请先上传自己的知识文件，并且文件中不含某些特殊字符，否则将返回error. <br>
+        3. 项目处于建设初期，请多多包含！有任何使用问题，请通过[问题交流区](https://modelscope.cn/studios/thomas/ChatYuan-test/comment)或[Github Issue区](https://github.com/thomas-yanxin/LangChain-ChatGLM-Webui/issues)进行反馈. <br>
+        </font>""")
     demo.queue().launch(share=False)
